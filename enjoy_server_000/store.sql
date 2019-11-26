a0f9fc15ad8b52d00a0abf20446a579168538397
hancongcong <1069435923@qq.com>
2019-11-26 10:11:15 +0800

    third

---
+++ enjoy_server_000/store.sql
@@ -0,0 +1,22 @@
+-- SET NAMES UTF8;
+-- USE ey;
+-- CREATE TABLE ey_store(
+--  id INT PRIMARY KEY AUTO_INCREMENT,
+--  title VARCHAR(255),
+--  subtitle VARCHAR(64),
+--  price DECIMAL(10,2),
+--  dprice DECIMAL(10,2),
+--  pic VARCHAR(64)
+-- );
+INSERT INTO ey_store VALUES(null,"炭匠炉端烧·酒（金融街购物中心）单人餐","风行北海道的炉端烧料理",156,312,"http://127.0.0.1:3000/s1.jpg");
+INSERT INTO ey_store VALUES(null,"桥场居酒屋 IZAKA-YA 桥场居酒屋全天单人餐","重现昭和建筑风格的精致居酒屋",55,null,"http://127.0.0.1:3000/s2.jpg");
+INSERT INTO ey_store VALUES(null,"鳗富士正宗活鳗鱼料理单/双人餐","多款活鳗料理一次尝鲜",168,258,"http://127.0.0.1:3000/s3.jpg");
+INSERT INTO ey_store VALUES(null,"千羽鹤海鲜居酒屋（大望路店）双人小赏套餐","蓝鳍金枪鱼主题超值套餐",288,400,"http://127.0.0.1:3000/s4.jpg");
+INSERT INTO ey_store VALUES(null,"尚水长廊铁板烧餐厅 （三里屯太古里店）全天单人餐","午餐与晚餐通用",368,725,"http://127.0.0.1:3000/s5.jpg");
+INSERT INTO ey_store VALUES(null,"凤寿司午市海鲜饭套餐","府学胡同里的和风美馐",158,228,"http://127.0.0.1:3000/s6.jpg");
+INSERT INTO ey_store VALUES(null,"秋本 AKIMOTO（二店）单人手握寿司套餐","每周三次从日本长崎空运生鲜",200,260,"http://127.0.0.1:3000/s7.jpg");
+INSERT INTO ey_store VALUES(null,"炉匠炉端烧双人餐","采用专业北海道备长炭烤制",188,295,"http://127.0.0.1:3000/s8.jpg");
+INSERT INTO ey_store VALUES(null,"美豚 Biton 单人午餐","9款人气定食搭配咖啡、沙拉",38,58,"http://127.0.0.1:3000/s9.jpg");
+INSERT INTO ey_store VALUES(null,"一元居酒屋 MOTO 单人鳗鱼饭","当日现杀鲜活鳗鱼入料",158,231,"http://127.0.0.1:3000/s10.jpg");
+INSERT INTO ey_store VALUES(null,"Ishikawa 石川料理亭单人晚餐","尽享炭烧鳗鱼与日式小火锅",398,510,"http://127.0.0.1:3000/s11.jpg");
+INSERT INTO ey_store VALUES(null,"Ishikawa 石川料理亭单人午餐","七道式丰盛日料会席",198,320,"http://127.0.0.1:3000/s12.jpg");
---
+++ vue_cli_000/mydemo/src/components/enjoy/Storeh.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>  <!--唯一父元素-->
+       <!--1.导航条-->
+       <Header></Header>
+       <!--2.大图片-->
+
+
+       <!--3.店铺信息-->
+
+
+  </div>
+</template>
+
+<script>
+import header from "../enjoy/header.vue"
+export default {
+    data(){
+        return {
+            
+        }
+    },
+    methods:{
+
+    },
+  created(){
+
+  },
+components:{
+    "Header":header  //头部
+    
+   }
+
+}
+</script>
+
+<style>
+
+</style>
