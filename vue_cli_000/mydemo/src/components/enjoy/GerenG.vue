732f1bb11a31ddda1d526c13234531df6c08f3bf
lll <1841111@qq.com>
2019-12-07 10:47:47 +0800

    gw

--- vue_cli_000/mydemo/src/components/enjoy/GerenG.vue
+++ vue_cli_000/mydemo/src/components/enjoy/GerenG.vue
@@ -3,12 +3,12 @@
     <div class="GerenG1">
       <img src="../../../static/gerenG3.png" alt>
       <span>我的购物车</span>
-      <span class="GerenG2" @click="delm">清空购物车</span>
+      <span class="GerenG2">清空购物车</span>
     </div>
 
     <div class="G-F">
       <div style="margin:auto">
-        <van-checkbox v-model="checked" @change="itemchange"></van-checkbox>
+        <van-checkbox v-model="checked"></van-checkbox>
       </div>
       <div class="G-F1" @click="check">
         <div class="G-F2">
@@ -31,7 +31,7 @@
           </div>
         </div>
         <div style="margin:auto 0">
-          <van-button round type="danger" size="mini" @click="delm">删除</van-button>
+          <van-button round type="danger" size="mini">删除</van-button>
         </div>
       </div>
     </div>
@@ -80,72 +80,14 @@ export default {
         this.checked = !this.checked;
       }
     },
-    itemchange() {
-      //功能:商品复选框状态修改
-      //1:累加商品状态
-      //2:创建变量
-      var sum = 0;
-      //3:创建循环遍历商品状态
-      for (var item of this.list) {
-        if (item.cb) {
-          sum++;
-        }
-      }
-      //console.log(sum);
-      //console.log(this.list.length)
-      //4:如果true变量数据加一
-      //5:如果选中数量与数组相同，将
-      //  全选allcb=true
-      if (sum == this.list.length) {
-        this.allcb = true;
-      } else {
-        this.allcb = false;
-      }
-    },
-    delm() {
-      Dialog.confirm({
-        title: "标题",
-        message: "弹窗内容"
-      })
-        .then(res=>{
-    //2:选择确认
-    //3:创建空字符串 "1,2,3"
-    var str = "";
-    //4:创建循环遍历list数组
-    //  判断如果当前对象cb值为true
-    //  将对象id拼接
-    for(var item of this.list){
-        if(item.cb){
-          str += item.id+","
-        }
-    }
-    //5:判断用户是否选中商品
-    //  提示请选择要删除商品
-    if(str.length==0){
-      this.$messagebox("请选择要删除商品");
-      Dialog({ message: "请选择要删除商品" })
-      return;
-    }
-    //0            启始下标 8,9,
-    //str.length-1 保存几个字符串
-    str = str.substring(0,str.length-1);
-    console.log(str);//!!!!!!
-    //6:创建url创建obj 10:28 
-    var url = "delm";
-    var obj = {id:str};
-    //7:发送ajax请求
-    this.axios.get(url,{params:obj}).then(res=>{
-      //console.log(res)
-    //8:再新加载购物列表 
-     this.loadMore();
-    //9:提示删除成功
-     this.$toast("删除成功")
-    })
-    }).catch(err=>{
-    })
-    },
     loadMore(){
-      var url=""
+      //1:创建url
+      var url = "findcart"
+      //2:发送ajax请求获取购物车
+      this.axios.get(url).then(res=>{
+        console.log(res.data)
+      })
+      //3:将服务器返回数据保存list
     }
   }
 };
